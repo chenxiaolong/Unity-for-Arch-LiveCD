6d14ea3413fb2fb8a2156201e34f9ade3b97204d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-02-18 06:50:49 -0500

    Add simple build wrapper

---
+++ create-livecd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "x$(whoami)" != "xroot" ]; then
+  echo "This script must be run as root!"
+  exit 1
+fi
+
+pushd unity &>/dev/null
+
+./build.sh -N Unity-for-Arch        \
+           -V $(date '+%Y.%m.%d')   \
+           -L UFA_$(date '+%Y%m%d') \
+           -v
+
+popd &>/dev/null
